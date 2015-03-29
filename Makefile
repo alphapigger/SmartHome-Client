a3a6e7167886c9d2459825bed0e37daf62230dfb
Steven Han <steven.hl.0901@gmail.com>
2015-03-29 16:41:24 +0800

    fix bug

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ help:
 	@echo '												'
 	@echo 'Usage:										'
 	@echo '   make requirements	install requirements	'
+	@echo '   make deamon set supervisor cfg'
 
 requirements:
 	pip install -q -r requirements.txt -i https://pypi.python.org/simple
+
+deamon:
+	echo 'supervisord' | tee -a /etc/rc.local
+	cp supervisor/client.ini /etc/supervisor.d/
--- tool/network.py
+++ tool/network.py
@@ -5,6 +5,7 @@
 import threading
 import time
 import logging
+import os
 
 from lib.ping import network_test
 
@@ -17,12 +18,27 @@ def manage():
         if network_test('www.baidu.com'):
             logger.info('network is ok')
             if network_status == 0:
+                restart_client()  # restart client
                 network_status = 1
                 time.sleep(60)
         else:
             logger.error('network error')
             network_status == 0
             time.sleep(10)
+
+
+def restart_client():
+    os.system('''ps -ef | grep bin/start.py | grep -v grep | awk '{print
+        $2}'  | sed -e "s/^/kill -9 /g" | sh -''')
+    os.system('''cd /data/raspberry-pi-wechat-client && PYTHONPATH=. python
+        bin/start.py &''')
+    logger.info('restart client ...')
+
+
+def stop_client():
+    os.system('''ps -ef | grep bin/start.py | grep -v grep | awk '{print
+        $2}'  | sed -e "s/^/kill -9 /g" | sh -''')
+    logger.info('stop client ...')
 
 
 def deamon():
