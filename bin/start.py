702b64388ac291b6f0ac8e81c35bf463b4f98be4
Steven Han <steven.hl.0901@gmail.com>
2015-03-29 16:52:26 +0800

    fix bug

--- bin/start.py
+++ bin/start.py
@@ -3,6 +3,5 @@
 
 import client.client as client
 
-
 if __name__ == '__main__':
     client.start()
--- tool/network.py
+++ tool/network.py
@@ -28,10 +28,8 @@ def manage():
 
 
 def restart_client():
-    os.system('''ps -ef | grep bin/start.py | grep -v grep | awk '{print
-        $2}'  | sed -e "s/^/kill -9 /g" | sh -''')
-    os.system('''cd /data/raspberry-pi-wechat-client && PYTHONPATH=. python
-        bin/start.py &''')
+    os.system("ps -ef | grep bin/start.py | grep -v grep | awk '{print $2}'  | sed -e 's/^/kill -9 /g' | sh -")
+    os.system("cd /data/raspberry-pi-wechat-client && PYTHONPATH=. python bin/start.py &")
     logger.info('restart client ...')
 
 
