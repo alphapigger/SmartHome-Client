359e7c020a4a7c2303085a6af615314d98b88437
steven-hl <steven.hl.0901@gmail.com>
2015-04-09 22:56:54 +0800

    ad serial test files

---
+++ test/serial/serial_1.py
@@ -0,0 +1,20 @@
+import serial
+
+
+def hexShow(argv):
+    result = ''
+    hLen = len(argv)
+    for i in xrange(hLen):
+        hvol = ord(argv[i])
+        hhex = '%02x' % hvol
+        result += hhex + ' '
+    print 'hexShow:', result
+
+t = serial.Serial('com12', 9600)
+print t.portstr
+strInput = raw_input('enter some words:')
+n = t.write(strInput)
+print n
+str = t.read(n)
+print str
+hexShow(str)
