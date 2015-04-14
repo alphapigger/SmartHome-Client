175699fd208936ff7cd556efd48e64a29d225ce7
steven-hl <steven.hl.0901@gmail.com>
2015-04-14 23:01:26 +0800

    修复网络良好状态下，客户端断开无法重新启动

--- tool/network.py
+++ tool/network.py
@@ -21,6 +21,9 @@ def manage():
                 restart_client()  # restart client
                 network_status = 1
                 time.sleep(60)
+            elif network_status == 1:
+                # 需要检测client是否存在，不存在则重启
+                client_exists()
         else:
             logger.error('network error')
             network_status == 0
@@ -28,9 +31,19 @@ def manage():
 
 
 def restart_client():
-    os.system("ps -ef | grep bin/start.py | grep -v grep | awk '{print $2}'  | sed -e 's/^/kill -9 /g' | sh -")
-    os.system("cd /data/raspberry-pi-wechat-client && PYTHONPATH=. python bin/start.py &")
+    os.system("ps -ef | grep bin/start.py | grep -v grep | awk '{print $2}'"
+              "| sed -e 's/^/kill -9 /g' | sh -")
+    os.system("cd /data/raspberry-pi-wechat-client && PYTHONPATH=. "
+              "python bin/start.py &")
     logger.info('restart client ...')
+
+
+def client_exists():
+    count = int(os.popen("ps -ef | grep bin/start.py | grep -v grep"
+                         "| wc -l").read())
+    time.sleep(1)
+    if count == 0:
+        restart_client()
 
 
 def stop_client():
