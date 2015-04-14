e9a0467aeba4039eee9c8dd309f638cf5bbf491c
steven-hl <steven.hl.0901@gmail.com>
2015-04-14 21:40:46 +0800

    enhance

--- client/manage/manager.py
+++ client/manage/manager.py
@@ -13,6 +13,10 @@ class ClinetManager(object):
     port = "/dev/ttyAMA0"
     rate = 38400
 
+    CMD_REGISTER = 1   # 数据格式为 1 device_id device_type
+    CMD_HUMTEM = 2     # 数据格式  2 device_id hum tem
+    CMD_LED = 3        # 数据格式 3 device_id 0/1
+
     def __init__(self):
         self.com = serial.Serial(self.port, self.rate)
 
@@ -20,10 +24,6 @@ class ClinetManager(object):
         data = self.com.readline()
         logger.info('Receive msg : %r', data)
         return data
-
-    def receive(self):
-        while True:
-            self.read()
 
     def write(self, msg):
         msg += "\\"
@@ -49,8 +49,19 @@ def control(msg):
             return None, None
 
 
-# def start_monitor():
-#     manager.receive()
+def start_monitor():
+    while True:
+        data = manager.read()
+        data = data.split()
+        command = data[0]
+        if command == manager.CMD_REGISTER:
+            device_id = data[1]
+            device_type = data[2]
+        elif command == manager.CMD_HUMTEM:
+            device_id = data[1]
+            hum = data[2]
+            tem = data[3]
+
 
 
 def main():
--- client/msg_handler.py
+++ client/msg_handler.py
@@ -1,8 +1,7 @@
 # coding:utf-8
 
-import time
 import json
-# import signal
+import signal
 import logging
 
 # from .sensor import ht
@@ -17,17 +16,14 @@ class MessageHandler(object):
         self.info = info
 
     def handle(self):
-        msg_action = {'LIGHT_ON': self.lightHandle,
-                      'LIGHT_OFF': self.lightHandle,
-                      'REAL_TEMPERATURE': self.temperatureHandle,
+        msg_action = {'LIGHT_ON': self._light_handle,
+                      'LIGHT_OFF': self._light_handle,
+                      'REAL_TEMPERATURE': self._temperature_handle,
                       }
 
         return msg_action[self.key]()
 
-    def lightHandle(self):
-        # response = {'status': 0, 'err_msg': '', 'info': self.key}
-        # time.sleep(1)
-        # return json.dumps(response)
+    def _light_handle(self):
         if self.key == 'LIGHT_ON':
             command = 'light on'
             info = 'opening'
@@ -38,38 +34,30 @@ class MessageHandler(object):
         response = {'status': 0, 'err_msg': '', 'info': info}
         return json.dumps(response)
 
-    def temperatureHandle(self):
-        # def _timeout_handler(signum, frame):
-        #     raise AssertionError
-        # try:
-        #     signal.signal(signal.SIGALRM, _timeout_handler)
-        #     signal.alarm(2)  # 超时时间为2秒
-        #     humidity, temperature = ht.get_data()
-        #     signal.alarm(0)
-        #     response = {'status': 0,
-        #                 'err_msg': '',
-        #                 'info': {'humidity': humidity,
-        #                          'temperature': temperature},
-        #                 }
-        #     return json.dumps(response)
-        # except AssertionError:
-        #     logger.info('Acquire humidity and temperature timeout')
-        #     response = {'status': -1,
-        #                 'err_msg': '请检查温度湿度传感器的连接',
-        #                 'info': '温度湿度传感器或许未连接'}
-        #     return json.dumps(response)
+    def _temperature_handle(self):
+        def _timeout_hander(signum, frame):
+            raise AssertionError
 
-        # humidity, temperature = ht.get_data()
-        humidity, temperature = manager.control("humtem")
-        # humidity, temperature = None, None
-        if humidity is None or temperature is None:
+        try:
+            signal.signal(signal.SIGALRM, _timeout_hander)
+            signal.alarm(3)
+            humidity, temperature = manager.control("humtem")
+            signal.alarm(0)
+        except AssertionError:
+            logger.error('get temperature and humidity timeout...')
             response = {'status': -1,
                         'err_msg': 'ERROR_NODATA',
                         'info': '小兵正在拼死拼活获取数据中，请耐心等待...'}
         else:
-            response = {'status': 0,
-                        'err_msg': '',
-                        'info': {'humidity': humidity,
-                                 'temperature': temperature},
-                        }
+            if humidity is None or temperature is None:
+                response = {'status': -1,
+                            'err_msg': 'ERROR_NODATA',
+                            'info': '小兵正在拼死拼活获取数据中，请耐心等待...'}
+            else:
+                response = {'status': 0,
+                            'err_msg': '',
+                            'info': {'humidity': humidity,
+                                     'temperature': temperature},
+                            }
+
         return json.dumps(response)
